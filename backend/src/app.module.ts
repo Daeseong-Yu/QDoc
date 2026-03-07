1bb22d673af34db83feacf31a9e8426730d00c5a
Daeseong Yu <dyu8422@conestogac.on.ca>
2026-03-07 08:06:38 -0500

    Add symptoms and queue APIs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 
 import { HospitalsModule } from './hospitals/hospitals.module'
+import { QueueModule } from './queue/queue.module'
+import { SymptomsModule } from './symptoms/symptoms.module'
 
 @Module({
-  imports: [HospitalsModule],
+  imports: [HospitalsModule, SymptomsModule, QueueModule],
 })
 export class AppModule {}

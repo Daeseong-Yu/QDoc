828893ad8f0fe5c9835243f97b86d00ef71c8763
Daeseong Yu <dyu8422@conestogac.on.ca>
2026-03-07 02:42:16 -0500

    add front-end landing page layout

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,7 +1,9 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { HomePage } from '../pages/HomePage'
+import { HospitalSearchPage } from '../pages/HospitalSearchPage'
 import { LoginPage } from '../pages/LoginPage'
+import { SymptomPage } from '../pages/SymptomPage'
 import { ProtectedRoute } from './ProtectedRoute'
 
 export function AppRouter() {
@@ -11,6 +13,8 @@ export function AppRouter() {
 
       <Route element={<ProtectedRoute />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/hospitals" element={<HospitalSearchPage />} />
+        <Route path="/symptoms" element={<SymptomPage />} />
       </Route>
 
       <Route path="*" element={<Navigate to="/" replace />} />

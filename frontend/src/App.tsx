fef04adc210ea7f8e7f02e004f999d8291dae0e8
Daeseong Yu <dyu8422@conestogac.on.ca>
2026-03-07 00:51:53 -0500

    Setting up the front end

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <main className="app-root">
+      <div className="app-shell">
+        <h1>QDoc</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
